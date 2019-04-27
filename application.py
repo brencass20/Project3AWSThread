b8a2c78f4c1a257b0542f568cc0c41f83cf637ae
brencass20 <brendancassidy@email.arizona.edu>
2019-04-27 00:53:47 +0000

    This commit is for all working files.  Passwords are left out.

--- application.py
+++ application.py
@@ -1,11 +1,9 @@
-"""
-Project 3: CSC 346, adapting the "Threads" application we had first created
-to use Flask and then add images to a database.
+#! /usr/bin/env python3
+"""e Flask and then add images to a database.
 """
 
 import MySQLdb
 import private_no_share_dangerous_passwords as pnsdp
-import secrets
 import json
 import random
 import requests
@@ -13,15 +11,17 @@ import time
 from flask import Flask, request, render_template, url_for, redirect, make_response, g
 app = Flask(__name__)
 
+globalThreadId=0
+
 
 def get_db():
 	#The DB is storest in the application contect.
-	my.db = conn.MySQLdb.connect(host=pnsdp.DB_HOST,user=pnsdp.DB_USER,passwd=pnsdp.DB_PASSWORD, db=pnsdp.DB_DATABASE)
-	return my.db
+	db = MySQLdb.connect(host=pnsdp.DB_HOST,user=pnsdp.DB_USER,passwd=pnsdp.DB_PASSWORD, db=pnsdp.DB_DATABASE)
+	return db
 
-def execute_sql_insert(database, sql, val):
+def execute_sql_insert(database, sql, val=None):
 	cursor=database.cursor()
-	if Val == None:
+	if val == None:
 		cursor.execute(sql)
 	else:
 		cursor.execute(sql, val)
@@ -31,7 +31,8 @@ def execute_sql_insert(database, sql, val):
 
 def execute_sql_select(database, sql):
 	cursor=database.cursor()
-	allSelects = cursor.execute(sql).fetchall()
+	cursor.execute(sql)
+	allSelects = cursor.fetchall()
 	cursor.close()
 	database.close()
 	return allSelects
@@ -39,44 +40,27 @@ def execute_sql_select(database, sql):
 	
 @app.route('/', methods=['GET','POST'])
 def index():
-	#if request.method == 'POST' and request.form["id"] is not None :
-	#	id = request.form["id"]
-	#	print(id)
-	#	if "," not in id:
-	'''sql = """SELECT * FROM comments WHERE threadId=%s;""" % (id)
+	if "userName" in request.form.keys():
+		#Create the form before loading the page
+		userName = request.form["userName"]
+		postString = request.form["postString"]
+		id = random.randint(0,10000)
+		sql = """INSERT INTO threads (threadId, threadContent, threadAuthor, threadLikes) VALUES (%s,%s, %s, %s)""" 
+		val = (id, postString, userName, "0")
+		execute_sql_insert(get_db(), sql,val)
+		
+	sql = """SELECT * FROM threads WHERE threadId IS NOT NULL;"""
 	sqlTot = execute_sql_select(get_db(),sql)
 	totList = []
 	for row in sqlTot:
-	commentId=row[1]
-	comment=row[2]
-	author=row[3]
-	likes=int(row[4])
-	lineString={"threadid":id,"likes":likes,"author":author,"comment":comment,"commentid":commentId}
-	totList.append(lineString)
-	'''
-	#		totList=[]
-	#		likes=1
-	#		comment="TestMessage"
-	#		author="Brendan"
-	#		threadid=-1
-	#		commentid=1
-	#		lineString={"threadid":id,"likes":likes,"author":author,"comment":comment,"commentid":commentid}
-	#		totList.append(lineString)
-	#		return render_template("thread.html",msg="Test Thread",list=totList)
-	#	else:
-	#		return redirect(url_for("create_comment.html" ), 303)#,threadid=id)
-	#else:
-	#'''sql = """SELECT * FROM threads WHERE threadId IS NOT NULL;"""
-	#sqlTot = execute_sql_select(get_db(),sql)
-	#totList = []
-	#for row in sqlTot:
-	#	id=int(row[0])
-	#	post=row[1]
-	#	author=row[2]
-	#	likes=int(row[3])
-	#	lineString={"id":id,"likes":likes,"author":author,"message":post}
-	#	totList.append(lineString)
-	#'''
+		id=int(row[0])
+		post=row[1]
+		author=row[2]
+		likes=int(row[3])
+		lineString={"id":id,"likes":likes,"author":author,"message":post}
+		totList.append(lineString)
+	
+	'''TESTING LOCALLY: 
 	totList=[]
 	likes=1
 	post="TestMessage"
@@ -84,6 +68,7 @@ def index():
 	id=-1
 	lineString={"id":id,"likes":likes,"author":author,"message":post}
 	totList.append(lineString)
+	totList.append(lineString)'''
 	return render_template("index.html", totStr="",list=totList)
 
 @app.route('/thread', methods=['GET','POST'])
@@ -95,15 +80,20 @@ def thread():
 		postString = request.form["postString"]
 		threadid = request.form["id"]
 		commentID = random.randint(1,10000)
-		'''
-		sql = """INSERT INTO comments (threadId,commentID, commentContent, commentAuthor, commentLikes) VALUES (%s,%s,%s, %s, %s)"""
-		val = (threadID, commentID, postString, userName, 0)
+		sql = """INSERT INTO comments (threadId,commentID, commentContent, commentAuthor, commentLikes) VALUES (%s,%s,%s,%s,%s)""" 
+		val =  (threadid,commentID,postString,userName,"0")
 		execute_sql_insert(get_db(), sql, val)
-		'''
-	
+	print("Inside Thread Redirect")	
+	print(request.form)
 	#All the code to generate the comments, and message  for a specific thread
-	#threadid = request.form["id"]
-	'''sql = """SELECT * FROM comments WHERE threadId=%s;""" % (id)
+	global globalThreadId
+	if globalThreadId == 0:#request.form["id"] is not None:
+		threadid = request.form["id"]
+	else:
+		print("Inside Else")
+		global globalThreadId
+		threadid = globalThreadId
+	sql = """SELECT * FROM comments WHERE threadId=%s;""" % (threadid)
 	sqlTot = execute_sql_select(get_db(),sql)
 	totList = []
 	for row in sqlTot:
@@ -111,9 +101,13 @@ def thread():
 		comment=row[2]
 		author=row[3]
 		likes=int(row[4])
-		lineString={"threadid":id,"likes":likes,"author":author,"comment":comment,"commentid":commentId}
+		lineString={"likes":likes,"author":author,"comment":comment,"commentid":commentId}
 		totList.append(lineString)
-	'''
+
+	sql = """ SELECT threadContent FROM threads WHERE threadId=%s;""" % (threadid)
+	sqlMessage=execute_sql_select(get_db(),sql)
+	sqlMessage=sqlMessage[0][0]
+	''' TESTING LOCALLY:
 	totList=[]
 	likes=1
 	comment="TestMessage"
@@ -121,47 +115,36 @@ def thread():
 	threadid=-1
 	commentid=1
 	lineString={"threadid":id,"likes":likes,"author":author,"comment":comment,"commentid":commentid}
-	totList.append(lineString)
-	return render_template("thread.html", msg="This is the thread",listcomment=list,threadid=threadid)
+	totList.append(lineString)'''
+	return render_template("thread.html",msg=sqlMessage,listcomment=totList,threadid=threadid)
 
 @app.route("/createthread", methods=['GET','POST'])
 def create_thread():
-	#Define the Capabilities of a the Create Thread.  Including the automatic redirect
-	if request.method == 'POST':
-		#Get data from form.
-		userName = request.form["userName"]
-		postString = request.form["postString"]
-		id = random.randint(0,10000)
-		'''sql = """INSERT INTO threads (threadId, threadContent, threadAuthor, threadLikes) VALUES (%s,%s, %s, %s)"""
-		val = (id, postString, userName, 0)
-		execute_sql_insert(get_db(), sql, val)'''
-		return redirect(url_for("index"), 303)
-	else:
-		return render_template("create_thread.html")
+	return render_template("create_thread.html")
 
 @app.route("/createcomment", methods=['GET','POST'])
 def create_comment():
-	return render_template("create_comment.html",threadid=request.form["id"])
+	threadid=request.form["id"]
+	return render_template("create_comment.html",threadid=threadid)
 
 
 #Updates database and then redirects to the original Page.
 @app.route('/upvote_thread', methods=["GET","POST"])
 def upvote_thread():
 	id=request.form["id"]	#Need to get the id num from form.
-	'''
 	sql = """UPDATE threads SET threadLikes = threadLikes + 1 WHERE threadId=%s""" % id
 	execute_sql_insert(get_db(), sql, None)
-	'''
 	return redirect(url_for("index"), code =303)
 
 @app.route('/upvote_comment', methods=["GET","POST"])
 def upvote_comment():
-	id=request.form["id"]	#need to get the id from form.
-	'''
+	idf=request.form["id"]	#need to get the id from form.
+	id=idf.split(',')[0]
+	global globalThreadId 
+	globalThreadId= idf.split(',')[1]
 	sql ="""UPDATE comments  SET commentLikes = commentLikes + 1 WHERE commentID=%s""" % id
 	execute_sql_insert(get_db(), sql, None)
-	'''
 	return redirect(url_for("thread"), 303)
 
 if __name__=='__main__':
-	app.run()
+	app.run(host='0.0.0.0',port=80)
